e9a0f5540cb2d71b429b26e7be9f5ea67cdae012
sebin <sebin@fegno.com>
2023-04-11 13:16:56 +0530

    our values added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,75 +12,23 @@ import ContactForm from "@/compnents/contact-form";
 import Button from "@/compnents/button";
 import HomepageNewsAndBlog from "@/compnents/homepage-news-blog";
 import HomepageFeatures from "@/compnents/homepage-features";
+import HomepageOurVision from "@/compnents/homepage-our-vision";
+import Container from "@/compnents/container";
+import HomepageOurValues from "@/compnents/homepage-our-values";
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
-      <section>
-        <div className="container py-5">
-          <div className="d-block card-glowing-blue p-5 rounded-20 animated-border-glow">
-            <div className="row mb-4">
-              <div className="col-8">
-                <Title align="left" title="Our Vision" />
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-12">
-                <h4>TO TRAIN YOU TODAY FOR THE OPPORTUNITIES OF TOMORROW</h4>
-              </div>
-              <div className="col-12">
-                <p>
-                  MADEIT is committed to offer an integrated training on the
-                  eight pillars of IR 4.0 to the people. Our unique position in
-                  the world of higher education is based on strong values:
-                  inclusivity, open culture, and always aiming at excellence.
-                  The ultimate goal is sustainable professional integration into
-                  the labor market
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      <section className="my-5 py-5">
+        <Container>
+          <HomepageOurVision />
+        </Container>
       </section>
-      <section>
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-6">
-              <div className="card-violet animated-border-glow rounded-30 p-4">
-                <span className="d-block">TO TRAIN YOU</span>
-                <span className="d-block">TODAY FOR THE</span>
-                <span className="d-block">OPPORTUNITIES OF</span>
-                <span className="d-block">TOMORROW</span>
-              </div>
-            </div>
-            <div className="col-12 col-md-6">
-              <div className="card-black animated-border-glow rounded-30 p-4">
-                <div>
-                  <p>
-                    At MADElT, we believe in differentiation and inclusivity.
-                    The education model focuses on project-based learning and
-                    teamwork, rather than theoretical education. As we integrate
-                    the latest educational innovations, our goal is to empower
-                    the next leading generation of IT talent.
-                  </p>
-                </div>
-                <div>
-                  <ul>
-                    <li>
-                      We have mentors and mentees instead of teachers and
-                      students
-                    </li>
-                    <li>Capstone ( project) -BASED PEDAGOGY</li>
-                    <li>PEER-TO-PEER LEARNING</li>
-                    <li>GAMIFICATION</li>
-                    <li>Chief technical officers as MENTORS</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <section className="my-5 pt-5">
+        <Container>
+          <HomepageOurValues />
+        </Container>
       </section>
       <section>
         <div className="container py-5">
@@ -111,7 +59,9 @@ export default function Home() {
       <section className="my-5">
         <HomepageFeatures />
       </section>
-      <ContactForm />
+      <section className="pt-5">
+        <ContactForm />
+      </section>
       <section>
         <HomepageNewsAndBlog />
       </section>
