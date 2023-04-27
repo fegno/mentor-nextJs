66d0edaccdc406912adb5f17cea06793b8ba49a7
Kshema Shylan <kshema@fegno.com>
2023-04-27 17:32:07 +0530

    style issues

--- src/pages/author-pad/index.tsx
+++ src/pages/author-pad/index.tsx
@@ -20,6 +20,9 @@ const Author : React.FC = () => {
                         <div className={style.author_dec}>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </div>
+                        {/* <div className={style.social_icon}>
+                            <img src={authsocial.src} alt="linkedIn" />
+                        </div> */}
                     </div>
                 </div>
             </div>
