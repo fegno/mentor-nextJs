04e479d3b4b86ccc6bd83d5a831c3d222b9426ce
KshemaS <kshema@fegno.com>
2023-04-28 17:19:17 +0530

    gallery style

--- src/compnents/gallery-card/index.tsx
+++ src/compnents/gallery-card/index.tsx
@@ -8,10 +8,10 @@ const GalleryCard : React.FC <GalleryProps> = ({data}) => {
     return(
         <div className={style.gallery_card_container}>
             <div className={"row"}>
-                <div className={`col-4 col-sm-3 ${style.gallery_img_wrapper}`}>
+                <div className={`col-4 col-md-3 ${style.gallery_img_wrapper}`}>
                     <img src={data.image} />
                 </div>
-                <div className={`col-8 col-sm-9 ${style.gallery_data_wrapper}`}>
+                <div className={`col-8 col-md-9 ${style.gallery_data_wrapper}`}>
                     <div className={style.gallery_card_title}>
                         {data.title}
                     </div>
