26eeb676f1049996cfbf1587b47df83cf1f33a6d
sebin <sebin@fegno.com>
2023-05-07 11:24:40 +0530

    responsive table added

--- src/compnents/comparison-table/index.tsx
+++ src/compnents/comparison-table/index.tsx
@@ -1,71 +1,129 @@
-import React from 'react'
+import React from "react";
 import style from "./ComparisonTable.module.scss";
 
 const ComparisonTable: React.FC = () => {
-    const TABLE_CONTENT = [
-        {
-            cell1: "Curriculum", cell2: "Industry vetted curriculum.redesigning with our R& D team consisting of industry experts who have more than 10yrs experience.", cell3: "Outdated"
-        },
-        {
-            cell1: "Instructors", cell2: "Our experienced instructors are having more than 10yrs experience in MNC’s with coding and development experience.", cell3: "No industry experience"
-        },
-        {
-            cell1: "Assigments", cell2: "50+ Apps and Products to be Coded", cell3: "Theoretical written papers"
-        },
-        {
-            cell1: "Mentorship", cell2: "Weekly sessions with industry experts", cell3: "No dedicated guidance"
-        },
-        {
-            cell1: "Internship", cell2: "Compulsory paid internship according to university semester structure for final year.", cell3: "No support for Internship Opportunities"
-        },
-        {
-            cell1: "Employability", cell2: "Job ready certificate", cell3: "Has to undergo extra training"
-        },
-        {
-            cell1: "Graduate", cell2: "Senior Development Executive", cell3: "Junior SDE"
-        },
-        {
-            cell1: "Live project experience", cell2: "Opportunity to work part time at SW development companies from 2nd yr after completing IIJRC prgrm", cell3: "Lack of resources"
-        },
-        {
-            cell1: "Add on cerification", cell2: "Vetted with industry- IIJRC", cell3: "Some local training programs not vetted with industry"
-        },
-        {
-            cell1: "Teaching Methodology", cell2: "Targeting students to pass exams with the knowledge of how these theories effectively is used in their SW careers.", cell3: "Following university curriculum same as school to get marks through theory studies."
-        },
-    ]
-    return (
-        <div className={style.comparison_table}>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Criteria &apos; s</th>
-                        <th>
-                            Mentor Approach
-                        </th>
-                        <th>Traditional Education</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {TABLE_CONTENT.map((row: any, index: number) => {
-                        return <tr key={index} className={style.table_row}>
-                            <td className={style.first_cell}>{row.cell1}</td>
-                            <td>
-                                <ul className={style.mentor_list}>
-                                    <li>{row.cell2}</li>
-                                </ul>
-                            </td>
-                            <td><ul className={style.others_list}>
-                                <li>
-                                    {row.cell3}
-                                </li>
-                            </ul></td>
-                        </tr>
-                    })}
-                </tbody>
-            </table>
-        </div>
-    )
-}
+  const TABLE_CONTENT = [
+    {
+      cell1: "Curriculum",
+      cell2:
+        "Industry vetted curriculum.redesigning with our R& D team consisting of industry experts who have more than 10yrs experience.",
+      cell3: "Outdated",
+    },
+    {
+      cell1: "Instructors",
+      cell2:
+        "Our experienced instructors are having more than 10yrs experience in MNC’s with coding and development experience.",
+      cell3: "No industry experience",
+    },
+    {
+      cell1: "Assigments",
+      cell2: "50+ Apps and Products to be Coded",
+      cell3: "Theoretical written papers",
+    },
+    {
+      cell1: "Mentorship",
+      cell2: "Weekly sessions with industry experts",
+      cell3: "No dedicated guidance",
+    },
+    {
+      cell1: "Internship",
+      cell2:
+        "Compulsory paid internship according to university semester structure for final year.",
+      cell3: "No support for Internship Opportunities",
+    },
+    {
+      cell1: "Employability",
+      cell2: "Job ready certificate",
+      cell3: "Has to undergo extra training",
+    },
+    {
+      cell1: "Graduate",
+      cell2: "Senior Development Executive",
+      cell3: "Junior SDE",
+    },
+    {
+      cell1: "Live project experience",
+      cell2:
+        "Opportunity to work part time at SW development companies from 2nd yr after completing IIJRC prgrm",
+      cell3: "Lack of resources",
+    },
+    {
+      cell1: "Add on cerification",
+      cell2: "Vetted with industry- IIJRC",
+      cell3: "Some local training programs not vetted with industry",
+    },
+    {
+      cell1: "Teaching Methodology",
+      cell2:
+        "Targeting students to pass exams with the knowledge of how these theories effectively is used in their SW careers.",
+      cell3:
+        "Following university curriculum same as school to get marks through theory studies.",
+    },
+  ];
+  return (
+    <div className={style.comparison_table}>
+      <table className={style.web_view}>
+        <thead>
+          <tr>
+            <th>Criteria &apos; s</th>
+            <th>Mentor Approach</th>
+            <th>Traditional Education</th>
+          </tr>
+        </thead>
+        <tbody>
+          {TABLE_CONTENT.map((row: any, index: number) => {
+            return (
+              <tr key={index} className={style.table_row}>
+                <td className={style.first_cell}>{row.cell1}</td>
+                <td>
+                  <ul className={style.mentor_list}>
+                    <li>{row.cell2}</li>
+                  </ul>
+                </td>
+                <td>
+                  <ul className={style.others_list}>
+                    <li>{row.cell3}</li>
+                  </ul>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className={style.mobile_view}>
+        <div className={style.title}>Comparison</div>
+        {TABLE_CONTENT.map((row: any, index: number) => {
+          return (
+            <div className={`${style.row}`} key={index}>
+              <div className="row">
+                <div className={`col-12 ${style.inner_title}`}>{row.cell1}</div>
+                <div className="col-12">
+                  <div className="row pb-4">
+                    <div className={`col-4 ${style.colllege_ttile}`}>
+                      Mentor Approach
+                    </div>
+                    <div className={`col-8 ${style.descrpition}`}>
+                      {row.cell2}
+                    </div>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="row">
+                    <div className={`col-4 ${style.colllege_ttile}`}>
+                      Traditional Education
+                    </div>
+                    <div className={`col-8 ${style.descrpition}`}>
+                      {row.cell3}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default ComparisonTable
+export default ComparisonTable;
--- src/compnents/homepage-courses-web/index.tsx
+++ src/compnents/homepage-courses-web/index.tsx
@@ -6,6 +6,8 @@ import CoursesTab from "../courses-tab";
 import Button from "../button";
 import CustomButton from "../custom-button";
 import { BsArrowRightCircle } from "react-icons/bs";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 type coursesProps = {
   courses: any;
@@ -25,6 +27,94 @@ const HomepageCoursesWeb: React.FC<coursesProps> = ({ courses, hasContainer }) =
     setIsModalOpen(false);
   };
   let course = courses[selectedCourse];
+
+  const particlesInit = async (main: any) => {
+    await loadFull(main);
+};
+
+
+const particlesOptions:any = {
+  background: {
+    color: {
+      value: "transparent",
+    },
+  },
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: false,
+        mode: "push",
+      },
+      onHover: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  fullScreen: {
+    enable: false,
+    zIndex: 0
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: false,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: false,
+    },
+    move: {
+      direction: "top",
+      enable: true,
+      outMode: "out",
+      random: false,
+      speed: 3,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 30,
+    },
+    opacity: {
+      value: 0.9,
+    },
+    shape: {
+      type: "edge",
+    },
+    size: {
+      random: true,
+      value: 3,
+    },
+  },
+detectRetina: true,
+}
+
   return (
     <div className={`${style.courses_wrapper} ${hasContainer ? style.small : ""}`}>
       <CustomModal isOpen={isModalOpen} close={handleCloseModal}>
@@ -58,7 +148,9 @@ const HomepageCoursesWeb: React.FC<coursesProps> = ({ courses, hasContainer }) =
       <div className={style.particle_canvas}>
         <div className={style.inner_wrapper}>
           <div className={style.title}> B.Tech</div>
-          <div className={style.particles}></div>
+          <div className={style.particles}>
+            <Particles init={particlesInit} options={particlesOptions} url="http://foo.bar/particles.json"/>
+          </div>
         </div>
       </div>
     </div>
