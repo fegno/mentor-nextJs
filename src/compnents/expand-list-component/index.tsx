37422d075c8ee0b4900ebd34b656b0d64cf2fd27
sebin <sebin@fegno.com>
2023-05-22 07:40:46 +0530

    changes

--- src/compnents/expand-list-component/index.tsx
+++ src/compnents/expand-list-component/index.tsx
@@ -1,23 +1,38 @@
 import React, { useEffect, useRef, useState } from "react";
 import style from "./ExpandListComponent.module.scss";
-
+import { gsap } from 'gsap';
 
 type expandListProps = {
   data: any;
-  title:string
-  expanded?: boolean
+  title: string;
+  expanded?: boolean;
 };
 
-const ExpandListComponent: React.FC<expandListProps> = ({ data, expanded ,title}) => {
-  const [showMenu, setShowMenu] = useState(false);
+const ExpandListComponent: React.FC<expandListProps> = ({
+  data,
+  expanded,
+  title,
+}) => {
+  const [showMenu, setShowMenu] = useState(true);
 
-  const listRef = useRef(null);
+  const listRef: any = useRef(null);
 
   useEffect(() => {
+    let list = listRef.current;
     if (expanded) {
-      setShowMenu(true)
+      setShowMenu(true);
     }
-  }, [])
+    gsap.from(list, {
+      opacity: 1,
+      duration: 1,
+      scrollTrigger: {
+        trigger: list,
+        start: 'top 10%', // Change this value as needed
+        end: 'bottom 80%', // Change this value as needed
+        toggleActions: 'play none none reverse',
+      },
+    });
+  }, []);
 
   const handleListShow = () => {
     setShowMenu(!showMenu);
@@ -33,7 +48,7 @@ const ExpandListComponent: React.FC<expandListProps> = ({ data, expanded ,title}
           {data?.map((list: any, index: number) => {
             return (
               <li className={style.list} key={`list${index}`}>
-                {list.jobs?list.jobs:list.courses}
+                {list.jobs ? list.jobs : list.courses}
               </li>
             );
           })}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import Layout from "@/compnents/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import gsap from "gsap";
 
 export default function App({ Component, pageProps }: AppProps) {
+  gsap.registerPlugin(ScrollTrigger)
   return (
     <Layout>
       <Component {...pageProps} />
